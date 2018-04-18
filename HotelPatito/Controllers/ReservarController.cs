239ac591b273276365dd653914f06c33f48fd184
Sergio2694 <sergdios09@gmail.com>
2018-04-18 17:44:15 -0600

    Insertar las pantallas de reservas

---
+++ HotelPatito/Controllers/ReservarController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelPatito.Controllers
+{
+    public class ReservarController : Controller
+    {
+        // GET: Reservar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Reservar
+        public ActionResult Sin_Resultados()
+        {
+            return View();
+        }
+    }
+}
